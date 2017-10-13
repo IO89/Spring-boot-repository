08ce09d6ca12b2cb5f31e25e0799ad968c04861d
IO89 <osipov.ivan.89@gmail.com>
2017-10-13 15:11:45 +0300

    problems with authorization

--- src/main/java/fi/hh/course/Library/web/BookController.java
+++ src/main/java/fi/hh/course/Library/web/BookController.java
@@ -4,6 +4,7 @@ import fi.hh.course.Library.domain.Book;
 import fi.hh.course.Library.domain.BookRepository;
 import fi.hh.course.Library.domain.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,7 +33,7 @@ public class BookController {
 	}
 
     //Authorization
-//    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
 	@RequestMapping(value = "/addbook")
     public String addBook(Model model){
 		model.addAttribute("book",new Book());
